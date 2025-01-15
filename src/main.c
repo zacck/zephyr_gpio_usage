fb18038c32cbe2759035a867c26cde9f9e0acdf3
Zacck <coderv63@gmail.com>
2025-01-15 09:25:55 +0200

    Add an interrupt to the rising edge of the button and control a different LED

--- src/main.c
+++ src/main.c
@@ -15,6 +15,7 @@
 #define LED0_NODE DT_ALIAS(led0)
 #define LED1_NODE DT_ALIAS(led1)
 #define SW0_NODE DT_ALIAS(sw0)
+#define LED2_NODE DT_ALIAS(led2)
 
 /*
  * A build error on this line means your board is unsupported.
@@ -23,6 +24,16 @@
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
 static const struct gpio_dt_spec sw0 = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+
+
+//add a callback for the gpio interrupt 
+void switch_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	gpio_pin_toggle_dt(&led2);
+}
+
+static struct gpio_callback switch_cb_data;
 
 int main(void)
 {
@@ -40,7 +51,19 @@ int main(void)
 
 	ret = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_INACTIVE);
 
+	ret = gpio_pin_configure_dt(&led2, GPIO_OUTPUT_INACTIVE);
+
 	ret = gpio_pin_configure_dt(&sw0, GPIO_INPUT);
+
+
+	// lets add an interrupt to the rising edge of the switch
+	gpio_pin_interrupt_configure_dt(&sw0, GPIO_INT_EDGE_RISING);
+
+	//initialize static callback struct
+	gpio_init_callback(&switch_cb_data, switch_pressed, BIT(sw0.pin));
+
+	// add the call back 
+	gpio_add_callback(sw0.port, &switch_cb_data);
 
 	while (1) {
 		ret = gpio_pin_toggle_dt(&led);
